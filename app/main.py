e290a07ec4e3170975181d7f6db7246d311374b8
Sebastián <seastanmora@gmail.com>
2025-03-15 06:21:08 -0300

    update

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from app.endpoints.default.index import default_router
 
-app = FastAPI()
+app = FastAPI(root_path="/")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(default_router)
