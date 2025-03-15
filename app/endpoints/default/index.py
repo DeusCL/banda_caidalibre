1a2bbf5fc094282a6f2fd223d9ae8a52968415fd
Sebastián <seastanmora@gmail.com>
2025-03-15 06:40:05 -0300

    test

--- app/endpoints/default/index.py
+++ app/endpoints/default/index.py
@@ -1,7 +1,17 @@
+from typing import Any
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/templates")
+
+def https_url_for(request: Request, name: str, **path_params: Any) -> str:
+    http_url = request.url_for(name, **path_params)
+
+    # Replace 'http' with 'https'
+    result = http_url.replace("http", "https", 1)
+    return result
+
+templates.env.globals["https_url_for"] = https_url_for
 
 default_router = APIRouter()
 
