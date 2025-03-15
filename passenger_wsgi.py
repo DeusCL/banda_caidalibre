aa661b263b56a5c82614550cb545a0c1bd99e634
Sebastián <seastanmora@gmail.com>
2025-03-15 05:39:17 -0300

    passenger update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 from app.main import app
 
 def run():
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 # Ensure Uvicorn runs in a separate thread
 if "RUN_MAIN" not in os.environ:
