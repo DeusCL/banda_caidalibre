939b907ba350dfcfa2d9ead570b83dbd1bc136d9
Sebastián <seastanmora@gmail.com>
2025-03-15 06:17:31 -0300

    update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
@@ -7,9 +5,6 @@ from app.endpoints.default.index import default_router
 
 app = FastAPI()
 
-# Get absolute path for static files
-static_dir = os.path.join(os.path.dirname(__file__), "static")
-
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(default_router)
 
