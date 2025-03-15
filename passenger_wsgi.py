415b0439c993b1203c2a467b43ade7043bfbb9dc
Sebastián <seastanmora@gmail.com>
2025-03-15 05:36:22 -0300

    passenger update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,17 @@
-from app.main import app
-from starlette.middleware.wsgi import WSGIApp
+import os
+import sys
+import threading
+import uvicorn
 
-application = WSGIApp(app)
+# Fix the path for imports
+sys.path.insert(0, os.path.dirname(__file__))
+
+from app.main import app
+
+def run():
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# Ensure Uvicorn runs in a separate thread
+if "RUN_MAIN" not in os.environ:
+    thread = threading.Thread(target=run, daemon=True)
+    thread.start()
