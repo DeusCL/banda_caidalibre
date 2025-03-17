32737b70059baf221e960f814470f59253b19301
Sebastián <seastanmora@gmail.com>
2025-03-16 23:07:14 -0300

    update

--- app/endpoints/default/index.py
+++ app/endpoints/default/index.py
@@ -4,11 +4,6 @@ from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/templates")
 
-def get_url_for(path) -> str:
-    return f"https://bandacaidalibre.com/{path}"
-
-templates.env.globals["get_url_for"] = get_url_for
-
 default_router = APIRouter()
 
 @default_router.get("/")
