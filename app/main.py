f9b79f5083fdfcbb01f6238f556797210afef9ea
Sebastián <seastanmora@gmail.com>
2025-03-17 03:29:16 -0300

    Hide /docs & /redoc

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from app.endpoints.default.index import default_router
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(default_router)
