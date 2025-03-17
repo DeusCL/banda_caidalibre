f14d65c3446e9df8929f98b54b58fcdebdba2f6e
Sebastián <seastanmora@gmail.com>
2025-03-17 03:49:04 -0300

    Added 404 not found error custom page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.endpoints.default.index import default_router
 
 app = FastAPI(docs_url=None, redoc_url=None)
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.exception_handler(404)
+async def not_found_handler(request: Request, exc):
+    return templates.TemplateResponse(
+        "errors/404.html",
+        {"request": request},
+        status_code=404
+    )
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(default_router)
